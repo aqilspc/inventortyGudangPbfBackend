fdc086db30b2183b428f0bebfc82d4347c41c69d
unknown <aqil@intermediatech.id>
2022-06-03 03:02:22 +0700

    add firebase and crud gudang

--- app/Http/Controllers/GudangController.php
+++ app/Http/Controllers/GudangController.php
@@ -1,24 +1,60 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
+use Illuminate\Http\Request;
 class GudangController extends Controller
 {
- public function get(){
 
- }
+    private $database;
 
- public function insert(){
-     
-}
+    public function __construct()
+    {
+        $this->database = \App\Services\FirebaseService::connect();
+    }
 
-public function update(){
-     
-}
+    public function get(){
+        return response()->json($this->database->getReference('gudang')->getValue());
+    }
 
-public function delete(){
-     
-}
+     public function detail($id){
+        $data = $this->database->getReference('gudang')->getValue();
+        //$data[$id];
+        return response()->json($data[$id]);
+    }
+
+
+    public function insert(Request $request){
+        $unique = strtotime(date('Y-m-d H:i:s'));
+        $this->database
+        ->getReference('gudang/' . $unique)
+        ->set([
+            'id_gudang'=>$unique,
+            'name' => $request->name,
+            'max_capacity' => $request->max_capacity,
+            'min_capacity' => $request->min_capacity,
+        ]);
+
+        return response()->json('warehouse has been created');
+    }
+
+    public function update(Request $request){
+        $this->database
+        ->getReference('gudang/' . $request->id_gudang)
+        ->update([
+            'name' => $request->name,
+            'max_capacity' => $request->max_capacity,
+            'min_capacity' => $request->min_capacity,
+        ]);
+
+        return response()->json('warehouse has been updated');
+    }
+
+    public function delete(Request $request){
+        $this->database
+        ->getReference('gudang/' . $request->id_gudang)
+        ->remove();
+
+        return response()->json('warehouse has been deleted');
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\GudangController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'not found';
 });
+
+Route::post('/api/gudang_insert', [GudangController::class, 'insert']);
+Route::get('/api/gudang_all', [GudangController::class, 'get']);
+Route::get('/api/gudang_by_id/{id}', [GudangController::class, 'detail']);
+Route::put('/api/gudang_update', [GudangController::class, 'update']);
+Route::delete('/api/gudang_delete', [GudangController::class, 'delete']);
