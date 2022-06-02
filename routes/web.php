b439dc4b55147f552b9160a1d0ca94d7501798b5
zulfikarrahman30 <izulrahman86@gmail.com>
2022-06-03 03:39:39 +0700

    adding fitur material

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GudangController;
+use App\Http\Controllers\MaterialController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,8 +18,17 @@ Route::get('/', function () {
     return 'not found';
 });
 
+//Route Gudang
 Route::post('/api/gudang_insert', [GudangController::class, 'insert']);
 Route::get('/api/gudang_all', [GudangController::class, 'get']);
 Route::get('/api/gudang_by_id/{id}', [GudangController::class, 'detail']);
 Route::put('/api/gudang_update', [GudangController::class, 'update']);
 Route::delete('/api/gudang_delete', [GudangController::class, 'delete']);
+
+//Route Material
+Route::post('/api/material_insert', [MaterialController::class, 'insert']);
+Route::get('/api/material_all', [MaterialController::class, 'get']);
+Route::get('/api/material_by_id/{id}', [MaterialController::class, 'detail']);
+Route::put('/api/material_update', [MaterialController::class, 'update']);
+Route::delete('/api/material_delete', [MaterialController::class, 'delete']);
+
